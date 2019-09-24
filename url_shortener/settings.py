fd9f2c288475c1a5d59285bdc108b154d5c72f3f
Victor H. Garcia <victorhugogarcia13d@gmail.com>
2019-09-24 00:16:52 -0500

    Add bootstrap library and define main base template

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'shortener.apps.ShortenerConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'url_shortener.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
